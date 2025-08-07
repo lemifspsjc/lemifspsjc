3bdbf03aab79fdf82c43372e9bff297d4b080d99
LEM - IFSP Campus São José dos Campos <lemifspsjc@gmail.com>
2025-08-07 16:05:20 +0000

    update consulta de materiais

--- assets/paginas/Materiais/consultaMateriais.js
+++ assets/paginas/Materiais/consultaMateriais.js
@@ -1,25 +1,5 @@
 // Função para exibir os dados no HTML
-function mostrarMateriais(lista) {
-  const container = document.getElementById("materiais");
-  container.innerHTML = ""; // Limpa antes de adicionar
 
-  lista.forEach(material => {
-    const div = document.createElement("div");
-    div.innerHTML = `
-      <strong>${material["ID_Material"]} - ${material["Material"]}</strong><br><br>
-      Quantidade Total: ${material["Quantidade Total"]}<br><br>
-      Armário: ${material["Armário"]}<br><br>
-      Prateleira: ${material["Prateleira"]}<br><br>
-      Área Trabalhada: ${material["Área Trabalhada"]}<br><br>
-      Habilidade(s): ${material["Habilidade(s) Trabalhada(s)"]}<br><br>
-      Descrição: ${material["Descrição do Material"]}
-      <br><br>
-      <hr>
-      <br>
-    `;
-    container.appendChild(div);
-  });
-};
 
 // URL da API do Google Apps Script
 const API_URL_CONSULTA_MATERIAIS = "https://script.google.com/macros/s/AKfycbxdC8WGd8afkiUr3WE98NzWKkFi9H220Lj7ZkVHjoxmQRow2F7LP8ITsoJqUgtagiU/exec";
